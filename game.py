857f90d827d06a74725413fd33805df908fdcbd9
Daniel Kazemian <achilles.7733@gmail.com>
2025-10-21 21:27:08 +0300

    Remove the timer & reordering the __init__ attribute. Also, solve the font problem

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import turtle
-import time
+from time import sleep
 from turtle import Screen
 from player import Player
 from ball import Ball
@@ -12,15 +12,17 @@ class Game:
         self.screen = Screen()
         self.BACKGROUND: str = "#221824"
         self.main_color: str = "#FF7B00"
-        self.game_font: tuple[str, int, str] = ("font/Pixeltype.ttf", 16, "bold") # Font is not working
+        self.game_font: tuple[str, int, str] = ("Pixeltype", 20, "bold") # Font is not working
 
         self.screen.screensize(400, 300, self.BACKGROUND)
         self.ga.title("Breakout Game")
-        # self.ga.Screen().bgcolor(self.BACKGROUND)
 
-        self.text_writer(text="Score: {Add The score}", align="left", x=-360, y=310)
-        self.text_writer(text="00:00:00", align="right", x=280, y=310) # I should connect my function to timer
-        self.ga.hideturtle() # this one remove the turtle after drawing
+        self.writer = turtle.Turtle()
+        self.writer.hideturtle()
+        self.writer.penup()
+        self.writer.color(self.main_color)
+
+        self.text_writer(text="Score: 0", align="left", x=-360, y=310)
 
         self.player: Player = Player()
         self.brick: Brick = Brick()
@@ -29,43 +31,48 @@ class Game:
 
         self.ball.constant_movement()
 
-        self.ga.done() # This one will come after self.ga.hideturtle()
         self.ga.mainloop()
 
     def text_writer(self, text: str, align: str, x: int, y: int) -> None:
-        self.ga.color(self.main_color)
-        self.ga.penup()
-        self.ga.goto(x, y)
-        self.ga.write(text, align=align, font=self.game_font)
+        self.writer.goto(x, y)
+        self.writer.clear()
+        self.writer.write(text, align=align, font=self.game_font)
 
 
-    def game_timer(self):
-        # TODO: two problems:
-              # todo 1: timer is current timer and it is not a timer
-              # todo 2: timer's refresh if too high which is like a fast blinking.
+    def game_timer(self) -> None: # I don't think I will use this function because the while loop will interrupt the game loop
+        """This function will count time from millisecond to minutes"""
+        millisecond: int = 0
+        second: int = 0
+        minute: int = 0
+        timer: str = f"{minute:02}:{second:02}:{millisecond:02}"
+
         while True:
-            self.ga.clear()  # This one clean the previous timer (00:00:00)
-            current_time: float = time.time()
-            local_time = time.localtime(current_time)
+            sleep(0.001)
+            if minute >= 3:
+                print("A chicken can do better then you")
 
-            minute = local_time.tm_min
-            second = local_time.tm_sec
-            microsecond: int = int((current_time - int(current_time)) * 1000000)
-            self.text_writer(text=f"{minute}:{second}:{microsecond}", align="right", x=280, y=310)
+            self.text_writer(text=timer, align="right", x=280, y=310)
+            millisecond += 1
 
-            self.screen.ontimer(self.game_timer, 1000)
+            if millisecond == 1000:
+                second += 1
+                millisecond = 0
+            if second == 60:
+                minute += 1
+                second = 0
 
 
-    # def game_border(self) -> None:
-    #     self.ga.speed(300)
-    #     self.ga.penup()
-    #     self.ga.goto(-420, -400)
-    #     self.ga.pendown()
-    #     self.ga.color(self.main_color)
-    #
-    #     self.ga.left(90)
-    #     for i in range(2):
-    #         self.ga.forward(770)
-    #         self.ga.right(90)
-    #         self.ga.forward(840)
-    #         self.ga.right(90)
+    def game_border(self) -> None:
+        """Drawing the game border"""
+        self.ga.speed(300)
+        self.ga.penup()
+        self.ga.goto(-420, -400)
+        self.ga.pendown()
+        self.ga.color(self.main_color)
+
+        self.ga.left(90)
+        for i in range(2):
+            self.ga.forward(770)
+            self.ga.right(90)
+            self.ga.forward(840)
+            self.ga.right(90)
